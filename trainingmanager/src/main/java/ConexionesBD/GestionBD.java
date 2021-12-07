f0ff8bbf072349bce8fee359178fe2dc099bbf9b
markez4400 <marquezbarbaraalex@gmail.com>
2021-12-07 20:41:36 +0100

    Create GestionBD.java

---
+++ trainingmanager/src/main/java/ConexionesBD/GestionBD.java
@@ -0,0 +1,22 @@
+package ConexionesBD;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class GestionBD {    // EN ESTA CLASE IRIA LAS FUNCIONES DE SQL PARA ACCEDER A LA BASE DE DATOS DESDE LA VARIABLE con
+    
+    Connection con = Conexion.conectar();
+    
+    Statement st;
+    ResultSet rs;
+    
+    public static void insertarEquipo()
+    {
+        
+    }
+    
+    
+    
+}
